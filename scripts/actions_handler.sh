7c56087b0017bec15ce0bc669fa60393e7f5f09b
Alex Cullen <acullen2929@gmail.com>
2023-01-24 16:57:35 -0500

    add actions handler script + refactored action

---
+++ scripts/actions_handler.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# Build Args from Inputs
+echo $EXTRA_ARGS
+
+case $COMMAND in
+'git')
+    cmd_args="${PATH}"
+    ;;
+
+'github')
+    cmd_args="--organization=${ORG}"
+    if [ ! -z $TOKEN ]
+    then
+        cmd_args+= "--token ${TOKEN}"
+    ;;
+
+'s3')
+    cmd_args="s3://${PATH}"
+    ;;
+esac
+
+if $FAIL
+then
+    base_cmd+="--fail "
+fi 
+
+if $JSON
+then 
+    base_cmd+="--json "
+fi
+
+CMD="${COMMAND} ${cmd_args} ${EXTRA_ARGS}"
+trufflehog $CMD --json >> $GITHUB_OUTPUT
